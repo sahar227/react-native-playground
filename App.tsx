41d04b3f5c0eb1ba49fd194d834f9674e2afd089
Sahar <sahar22798@gmail.com>
2024-01-14 18:15:49 +0200

    setup a blank ts project

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!!!
+      </Text>
+      <StatusBar style="inverted" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "red",
+  },
+});
