4a7af81df2e92a0b6cc4a97012ce2bfd261e6123
Sahar <sahar22798@gmail.com>
2024-01-19 17:27:11 +0200

    Use both stack and tabs components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import { Text } from "react-native";
 
 export default function Latout() {
   return (
     <>
       <Text className="pt-8">Main pages!</Text>
-      <Slot />
+      <Stack />
+      {/* <Slot /> */}
     </>
   );
 }
