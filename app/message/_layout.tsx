bd48b1067b15afe4ee50a48ee4e8387be17d68f1
Sahar <sahar22798@gmail.com>
2024-01-19 17:23:17 +0200

    Tried out Tabs and Stack components

--- app/message/_layout.tsx
+++ app/message/_layout.tsx
@@ -1,13 +1,19 @@
-import { Slot } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+// import { Slot } from "expo-router";
+// import { StatusBar } from "expo-status-bar";
+// import { Text, View } from "react-native";
+
+// export default function Layout() {
+//   return (
+//     <View>
+//       <Text>Message pages!</Text>
+//       <Slot />
+//       <StatusBar style="dark" />
+//     </View>
+//   );
+// }
+
+import { Tabs, Stack } from "expo-router";
 
 export default function Layout() {
-  return (
-    <View>
-      <Text>Message pages!</Text>
-      <Slot />
-      <StatusBar style="dark" />
-    </View>
-  );
+  return <Tabs />;
 }
