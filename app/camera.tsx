192e58ce2d3250a9d683ca3953171036a2c637af
Sahar <sahar22798@gmail.com>
2024-01-22 10:37:08 +0200

    basic expo camera usage

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,9 +1,42 @@
-import { Text, View } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function page() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
+  if (!permission) {
+    return (
+      <View>
+        <Button title="grant camera permission" onPress={requestPermission} />
+      </View>
+    );
+  }
+
+  if (!permission.granted) {
+    return (
+      <View>
+        <Text>Camera permission not granted.</Text>
+        <Button title="grant camera permission" onPress={requestPermission} />
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>Camera</Text>
+      <Camera className="h-[400px] w-[400px]" type={type}>
+        <View>
+          <TouchableOpacity onPress={toggleCameraType}>
+            <Text>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
